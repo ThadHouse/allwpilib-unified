1a665cafda27522113b24c81bc4772a91a94e959
Thad House <ThadHouse@users.noreply.github.com>
2018-04-19 11:25:53 -0700

    More work with cmake install

--- hal/hal-config.cmake
+++ hal/hal-config.cmake
@@ -2,4 +2,4 @@ include(CMakeFindDependencyMacro)
 find_dependency(wpiutil)
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/wpiHal.cmake)
+include(${SELF_DIR}/hal.cmake)
